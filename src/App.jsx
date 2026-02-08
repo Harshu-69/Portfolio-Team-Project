5ac16d95ad5d43751cdd193bcce8cc0e533431f0
Harshu-69 <khandalkarharsh668@gmail.com>
2026-02-08 18:08:57 +0530

    3rd commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className='w-screen min-h-screen text-white bg-[#111] ' >
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Navbar() {
+  return <div className="w-100vw h-16 bg-[#101319]  ">
+
+
+  </div>;
+}
+
+export default Navbar;
